27e118d24ce040e7214a369c3c046a79effdbd55
sandy-khalil <sandykhalil3322@gmail.com>
2026-04-07 19:04:59 +0200

    Refactor SSDMatcher match method and add private SSD calculation
    Updated match method to return a pair and added private method for SSD calculation.

--- src/backend/Module4_SSDMatching/SSDMatcher.h
+++ src/backend/Module4_SSDMatching/SSDMatcher.h
@@ -1,12 +1,25 @@
-#pragma once
+#ifndef SSDMATCHER_H
+#define SSDMATCHER_H
+
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include <tuple>
 
 class SSDMatcher {
 public:
-    static std::tuple<cv::Mat, std::vector<cv::DMatch>>
-    match(const cv::Mat& imgA, const cv::Mat& imgB, int topK, double ratio, bool crossCheck, int vizMode, double& timingMs) {
-        return {imgA.clone(), {}};
-    }
+    /**
+     * @return Pair of {Visualization Image, Vector of Matches}
+     */
+    static std::pair<cv::Mat, std::vector<cv::DMatch>> match(
+        const cv::Mat& imgA, 
+        const cv::Mat& imgB,
+        int topK, 
+        double ratio, 
+        bool crossCheck, 
+        int vizMode, 
+        double& timingMs);
+
+private:
+    static float calculateSSD(const cv::Mat& desc1, const cv::Mat& desc2);
 };
+
+#endif
